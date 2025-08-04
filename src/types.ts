ab74fcfb627ae41cb6455f76b3ed69a2e7b65c85
이태영 <lhlhgodz3@gmail.com>
2025-08-04 22:19:35 +0900

    :recycle:refactor: 초기 상품 및 쿠폰 상수 constants 폴더로 이동, type.ts 파일에 Notification 인터페이스 포함하여 업데이트
    constants > index.ts에 초기 상품과 쿠폰 상수를 위한 index.ts 파일을 생성했습니다.
    기본 App에 있는 initialProducts, initialCoupons 값을 이동하였습니다.
    메시지 처리를 위한 새로운 Notification 인터페이스를 types.ts의 Coupon 인터페이스에 포함하여 업데이트했습니다.

--- src/types.ts
+++ src/types.ts
@@ -19,6 +19,12 @@ export interface CartItem {
 export interface Coupon {
   name: string;
   code: string;
-  discountType: 'amount' | 'percentage';
+  discountType: "amount" | "percentage";
   discountValue: number;
+}
+
+export interface Notification {
+  id: string;
+  message: string;
+  type: "error" | "success" | "warning";
 }
